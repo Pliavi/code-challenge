a3e565f64eed66e2d9bc96964aff0543a16639da
Pliavi <pliavi.rpgm@gmail.com>
2018-12-24 13:33:17 -0200

    Bootstrap

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const pump = require("pump");
+const gulp = require("gulp");
+const styl = require("gulp-stylus");
+const pug = require("gulp-pug");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+
+gulp.task("copy", cb => {
+  pump(
+    [
+      gulp.src("src/img/*"),
+      gulp.dest("dest/img"),
+      gulp.src("src/css/**/*.css"),
+      gulp.dest("dest/css"),
+      gulp.src("src/js/**/*.js"),
+      gulp.dest("dest/js")
+    ],
+    cb
+  );
+});
+
+gulp.task("styl", cb => {
+  var plugins = [autoprefixer({ browsers: ["last 4 version"] }), cssnano()];
+
+  pump(
+    [
+      gulp.src("src/css/main.styl"),
+      styl(),
+      postcss(plugins),
+      gulp.dest("dest/css")
+    ],
+    cb
+  );
+});
+
+gulp.task("pug", cb => {
+  pump(
+    [
+      gulp.src(["src/**/*.pug", "!src/_includes/**/*"]),
+      pug(),
+      gulp.dest("dest")
+    ],
+    cb
+  );
+});
+
+gulp.task("watch", cb => {
+  gulp.watch(
+    ["src/img/**/*", "src/js/**/*.js", "src/css/**/*"],
+    gulp.series("copy")
+  );
+  gulp.watch("src/**/*.pug", gulp.series("pug"));
+  gulp.watch("src/css/**/*.styl", gulp.series("styl"));
+});
+
+gulp.task("default", gulp.parallel("pug", "styl", "copy", "watch"));
